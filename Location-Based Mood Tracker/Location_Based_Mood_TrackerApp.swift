c56de2432026749abb2e003274eb5a6a3a18e677
Isaiah Schatzline <isaiahschatzline@gmail.com>
2024-12-20 15:14:10 -0500

    Initial Commit

---
+++ Location-Based Mood Tracker/Location_Based_Mood_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Location_Based_Mood_TrackerApp.swift
+//  Location-Based Mood Tracker
+//
+//  Created by Isaiah Schatzline on 12/20/24.
+//
+
+import SwiftUI
+
+@main
+struct Location_Based_Mood_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
